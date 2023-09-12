124784ad456074c8d0f07a7fdf132cbbb0799090
breno.breves <brenocsb@dcc.ufrj.br>
2023-09-12 11:53:58 -0300

    Exercício 2?

--- aula-ts-01/exercício1.ts
+++ aula-ts-01/exercício1.ts
@@ -2,5 +2,5 @@ function somar(num1:number, num2:number) {
     return num1 + num2;
 }
 
-const resultado = somar("string", 3);
-console.log(resultado);
+const resultado = somar(1, 3);
+console.log(`Resultado: ${resultado}`);
